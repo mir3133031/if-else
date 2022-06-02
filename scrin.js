acca8b412c1861a6577844dbf9ce147851e08b97
мир <106350647+mir3133031@users.noreply.github.com>
2022-06-03 00:31:33 +0500

    Add files via upload

---
+++ scrin.js
@@ -0,0 +1,41 @@
+let name = prompt(`как тебя зовут`)
+if (name = `Alex`) {
+    let account = prompt(`номер счета`)
+    if (account == `7777`) {
+        let balance = + prompt(`Сколько обналичить`)
+        if (balance >= `10000`) {
+
+            let m   = (`жавобаш`)
+            if (m === `-`) {
+                alert(balance === balance)
+                let money = (`money`) 
+                if (money === true) {
+                    let remainder = prompt(`Сколько снял`)
+                } else {
+                    alert(
+                        `Не достаточно средств`
+                    )
+                }
+            } else {
+                alert(
+                    `Не достаточно средств`
+                )
+            }
+        } else {
+
+        }
+    } else {
+        alert(`Пользователь не найден досвидули`)
+    }
+
+} else {
+    alert(`Пользователь не найден досвидули`)
+
+
+}
+
+
+
+
+
+
